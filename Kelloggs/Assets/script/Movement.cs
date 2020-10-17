c2596626dcca15b5a0b25db89f033ba07b3b7dc3
Leo <leo.buono@ontariotechu.net>
2020-10-17 09:57:51 -0400

    controls started

---
+++ Kelloggs/Assets/script/Movement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Media;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public GameObject player;
+
+    float horizontalInput = 0;
+    float verticalInput = 0;
+    public float moveSpeed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+        
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        verticalInput = Input.GetAxis("Vertical");
+
+        player.transform.Translate(new Vector2(horizontalInput, verticalInput) * moveSpeed * Time.deltaTime);
+    }
+}
