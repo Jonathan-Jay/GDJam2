1b55801b4c06ed8439c71ad160bbe594fea186d2
Jonathan Jay <jonathan.jay@ontariotech.net>
2020-10-17 17:09:39 -0400

    not even our game lol

---
+++ Kelloggs/Assets/Scripts/Movement.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Movement : MonoBehaviour
+{
+    public TilemapCollider2D map;
+    public Camera cam;
+    public float speed;
+    private bool canMove = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canMove = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canMove)
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+                canMove = false;
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                GetComponent<Rigidbody2D>().velocity = Vector2.down * speed;
+                canMove = false;
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+                canMove = false;
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                 GetComponent<Rigidbody2D>().velocity = Vector2.left * speed;
+                canMove = false;
+            }
+            //name = "can move";
+            GetComponent<MeshRenderer>().material.color = Color.green;
+        }
+        else
+        {
+            //name = "no move";
+            GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+
+        cam.transform.position = transform.position + Vector3.back * 20;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        canMove = true;
+
+        Vector3 contactNormal = collision.GetContact(0).normal;
+        
+        if (map.IsTouchingLayers())
+        {
+            
+        }
+
+        if (contactNormal.x == 0)
+        {
+            if (contactNormal.y > 0)
+            {
+                transform.position += Vector3.up * 0.05f;
+            }
+            else
+            {
+                transform.position += Vector3.down * 0.05f;
+            }
+        }
+        else
+        {
+            if (contactNormal.x > 0)
+            {
+                transform.position += Vector3.right * 0.05f;
+            }
+            else
+            {
+                transform.position += Vector3.left * 0.05f;
+            }
+
+        }
+
+    }
+}
---
+++ Kelloggs/Assets/Scripts/Sensor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sensor : MonoBehaviour
+{
+    public List<GameObject> lostItems;
+    public Text itemSensor;
+    public float renderDistance;
+    private int currentIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentIndex = lostItems.Count - 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lostItems.Count == 0)
+        {
+            name = "Winner is you";
+        }
+        else{
+            float currentDistance = (lostItems[currentIndex].transform.position - transform.position).magnitude;
+
+            float percent = 1 - Mathf.Min(currentDistance, renderDistance) / renderDistance;
+
+            Text.defaultGraphicMaterial.color = new Color(1, 1, 1, Mathf.Sin(percent * Mathf.PI / 4f) * 2f);
+        }
+    }
+}
