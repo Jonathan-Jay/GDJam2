7930bfd6b9690a20ed0bb188a31c4e8af228c3d2
Jonathan Jay <jonathan.jay@ontariotech.net>
2020-10-17 17:47:09 -0400

    more scripts

---
+++ Kelloggs/Assets/Scripts/lookAt.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lookAt : MonoBehaviour
+{
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = transform.position - player.transform.position;
+        transform.rotation = Quaternion.AngleAxis( pos.x < 0 ? -Vector2.Angle(pos, Vector2.up) : Vector2.Angle(pos, Vector2.up), Vector3.back);
+        
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.position += Vector3.up * Time.deltaTime * 5f;
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.position += Vector3.down * Time.deltaTime * 5f;
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.position += Vector3.right * Time.deltaTime * 5f;
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.position += Vector3.left * Time.deltaTime * 5f;
+            }
+    }
+}
